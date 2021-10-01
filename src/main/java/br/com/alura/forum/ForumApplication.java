f21099096d93c1d9af5851e492bcf48d97dbc363
Reginaldo C Domingos <39439355+rcdomingos@users.noreply.github.com>
2021-10-01 08:39:09 -0300

    adicionado cache do spring

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,10 +2,12 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
+@EnableSpringDataWebSupport //habiltar a paginação padrão do spring
+@EnableCaching //habilitar o cache
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -31,6 +32,7 @@ public class TopicosController {
     CursoRepository cursoRepository;
 
     @GetMapping
+    @Cacheable(value = "lista")//value = id do cache
     public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
                                  @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable paginacao) {
         Page<Topico> topicos;
